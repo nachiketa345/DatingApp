03e21fa9caacea82434ee04481bc820c37cd70f6
Nachiketa anand <anand.nachiketa345@gmail.com>
2023-10-27 15:34:49 +0530

    Login and Register Interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_modules/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +11,22 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   title = 'Dating App';
-  users: any;
+  
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService:AccountService) {}
   ngOnInit(): void {
-    this.http.get('https://localhost:7281/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has been completed')
-      
-    })
+    
+    this.setCurrentUser();
 
+  }
+
+  
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user:User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 
   
